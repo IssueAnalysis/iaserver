f3bb6790e46b1319efe5d6c6f1c7c5821b05da23
ShortPupil <Zzh2625@Gmail.com>
2020-03-20 23:47:01 +0800

    一次大修改

--- src/main/java/com/issue/iaserver/IaserverApplication.java
+++ src/main/java/com/issue/iaserver/IaserverApplication.java
@@ -3,6 +3,7 @@ package com.issue.iaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**主入口*/
 @SpringBootApplication
 public class IaserverApplication {
 
--- src/main/java/com/issue/iaserver/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.issue.iaserver.service;
-
-import com.issue.iaserver.entity.User;
-
-public interface UserService {
-    User findById(long id);
-    int updateUser(User user);
-    int deleteById(long id);
-}
