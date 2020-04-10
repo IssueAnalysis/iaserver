1a92f90f928db8b889d873ec9636a25269463bb9
songjinze <734609160@qq.com>
2020-04-10 21:44:32 +0800

    尝试解决多模块下数据库连接问题

--- data_layer/src/main/java/com/iaserver/data/IaserverApplication.java
+++ data_layer/src/main/java/com/iaserver/data/IaserverApplication.java
@@ -2,14 +2,11 @@ package com.iaserver.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**主入口*/
 
-@Configuration
-@PropertySource(value = "classpath:application_data.properties")
 @SpringBootApplication
 public class IaserverApplication {
 
--- webserver/src/main/java/com/issue/iaserver/webserver/IaserverApplication.java
+++ webserver/src/main/java/com/issue/iaserver/webserver/IaserverApplication.java
@@ -3,19 +3,22 @@ package com.issue.iaserver.webserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**主入口*/
-@Configuration
-@PropertySource(value = "classpath:application_data.properties")
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class },
-        scanBasePackages = {"com.iaserver.data",
-                "com.issue.iaserver.webserver",
-                "com.issue.iaserver.nlp"})
+@SpringBootApplication
+@ComponentScan({"com.iaserver.data",
+        "com.issue.iaserver.webserver",
+        "com.issue.iaserver.nlp"})
+@EnableJpaRepositories("com.iaserver.data.mysql.dao")
+@EntityScan("com.iaserver.data.mysql.entity")
 public class IaserverApplication {
 
     public static void main(String[] args) {
