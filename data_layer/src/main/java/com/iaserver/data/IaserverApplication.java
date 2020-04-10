bbc2b6a26a5f5e50ae1aaba90c4c6362180c6136
songjinze <734609160@qq.com>
2020-04-10 18:16:06 +0800

    修改data_layer下的properties文件名

--- data_layer/src/main/java/com/iaserver/data/IaserverApplication.java
+++ data_layer/src/main/java/com/iaserver/data/IaserverApplication.java
@@ -3,10 +3,14 @@ package com.iaserver.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**主入口*/
+
+@Configuration
+@PropertySource(value = "classpath:application_data.properties")
 @SpringBootApplication
-@ComponentScan("com.iaserver.data")
 public class IaserverApplication {
 
     public static void main(String[] args) {
--- webserver/src/main/java/com/issue/iaserver/webserver/IaserverApplication.java
+++ webserver/src/main/java/com/issue/iaserver/webserver/IaserverApplication.java
@@ -6,13 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**主入口*/
+@Configuration
+@PropertySource(value = "classpath:application_data.properties")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class },
         scanBasePackages = {"com.iaserver.data",
                 "com.issue.iaserver.webserver",
                 "com.issue.iaserver.nlp"})
-
 public class IaserverApplication {
 
     public static void main(String[] args) {
